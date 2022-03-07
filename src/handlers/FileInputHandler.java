79f9a1ef40ddcf4830cccf3c0f6e5bc86bcba6ba
apchhiv <ira.karaseva.andre@gmail.com>
2022-03-07 19:48:22 +0300

    я забыла что там было

--- src/handlers/FileInputHandler.java
+++ src/handlers/FileInputHandler.java
@@ -3,6 +3,9 @@ package handlers;
 import java.util.ArrayList;
 
 public class FileInputHandler extends InputHandler {
+    /**
+     * Переопределённый метод для считывания с файла
+     */
     @Override
     public String read(boolean is) {
         return null;
