962337697cff46f62b57443133052d3d1e59bbef
apchhiv <ira.karaseva.andre@gmail.com>
2022-03-07 19:48:22 +0300

    парочка методов в AskInput и коммент

--- src/handlers/FileInputHandler.java
+++ src/handlers/FileInputHandler.java
@@ -1,6 +1,15 @@
 package handlers;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class FileInputHandler extends InputHandler {
+    /**
+     * Переопределённый метод для считывания с файла
+     * @return считанная строка без лишних пробелов
+     * @exception RuntimeException
+     */
     @Override
     public String read() {
         return null;
--- src/service/AskInput.java
+++ src/service/AskInput.java
@@ -1,17 +1,31 @@
 package service;
 
-import handlers.ConsoleInputHandler;
-import handlers.FileInputHandler;
+import handlers.InputHandler;
+import human_being.Coordinates;
 
-
+/**
+ * Класс, позволяющий переключать дружелюбный интерфейс для быстрой отладки
+ */
 public class AskInput {
     private static boolean friendlyInterface;
-    ConsoleInputHandler consoleInput = new ConsoleInputHandler();
-    FileInputHandler fileInput = new FileInputHandler();
+    private InputHandler inputHandler;
+    private Boolean booleanInput;
 
+    public AskInput(InputHandler inputHandler) {
+        this.inputHandler = inputHandler;
+    }
+
+    public void setInputHandler(InputHandler inputHandler){
+        this.inputHandler = inputHandler;
+    }
+
+    /**
+     * Метод, позволяющй включить дружелюбный интерфейс
+     * @throws RuntimeException
+     */
     public void turnOnFriendly() throws RuntimeException {
         System.out.println("Включить дружелюбный интерфейс?");
-        String input = consoleInput.read();
+        String input = inputHandler.read();
         input = input.toLowerCase();
         try {
             if (input.equals("true") || input.equals("yes") || input.equals("да")) {
@@ -27,37 +41,102 @@ public class AskInput {
         }
     }
 
-    public String askName() throws RuntimeException {
-        if (friendlyInterface) {
-            System.out.println("Введите имя: ");
-        }
-        String input = consoleInput.read();
-        try {
-            if (input == null) {
-                throw new RuntimeException("Строка не может быть пустой");
-            }
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-            askName();
-        }
-        return input;
+    /**
+     * Метод, позволяющий выключить дружелюбный интерфейс
+     */
+    public void turnOffFriendly(){
+        friendlyInterface = false;
     }
 
+    public String askName() {
+        printMessage("Введите имя: ");
+        return inputHandler.read();
+    }
+
+    /**
+     * Методы по полям класса HumanBeing для их считывания
+     * @return
+     * @throws RuntimeException
+     */
     public String askCoordinates() throws RuntimeException {
-        if (friendlyInterface) {
-            System.out.println("Введите координаты: ");
-        }
-        String input = consoleInput.read();
+        printMessage("Введите координаты: ");
+        String input = inputHandler.read();
         try {
-            if (input == null) {
-                throw new RuntimeException("Строка не может быть пустой");
+            if (!(input.contains(" "))) {
+                throw new RuntimeException("Пожалуйста, введите координаты через пробел.");
             }
+            int x = Integer.parseInt(input.substring(0, input.indexOf(" ")));
+            Float y = Float.parseFloat(input);
         } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-            askName();
+            if (friendlyInterface) {
+                System.out.println(e.getMessage());
+                askCoordinates();
+            }
         }
         return input;
     }
 
+    public Boolean askRealHero() throws RuntimeException {
+        printMessage("Является ли этот человек настоящим героем?");
+        String input = inputHandler.read();
+        input = input.toLowerCase();
+        try {
+            if (input.contains("true") || input.contains("yes") || input.contains("да")) {
+                booleanInput = true;
+            } else if (input.contains("false") || input.contains("no") || input.contains("нет") || input.equals("")) {
+                booleanInput = false;
+            } else {
+                throw new RuntimeException("Нераспознаваемые символы.");
+            }
+        } catch (RuntimeException e){
+            if (friendlyInterface){
+                System.out.println(e.getMessage());
+                askRealHero();
+            }
+        }
+        return booleanInput;
+    }
 
+    @SuppressWarnings("DuplicatedCode")
+    public Boolean askHasToothpick() throws RuntimeException {
+        printMessage("Есть ли у человека зубная щётка?");
+        String input = inputHandler.read();
+        input = input.toLowerCase();
+        try {
+            if (input.contains("true") || input.contains("yes") || input.contains("да")) {
+                booleanInput = true;
+            } else if (input.contains("false") || input.contains("no") || input.contains("нет") || input.equals("")) {
+                booleanInput = false;
+            } else {
+                throw new RuntimeException("Нераспознаваемые символы.");
+            }
+        } catch (RuntimeException e){
+            if (friendlyInterface){
+                System.out.println(e.getMessage());
+                askRealHero();
+            }
+        }
+        return booleanInput;
+    }
+
+    public int askImpactSpeed() {
+        printMessage("Введите скорость:");
+        String input = inputHandler.read();
+        return Integer.parseInt(input);
+    }
+
+    public String askSoundtrackName() throws RuntimeException {
+        printMessage("Введите название саундтрека:");
+        return inputHandler.read();
+    }
+
+    /**
+     * Внутренний метод для вывода сообщения относительно friendlyInterface
+     * @param message
+     */
+    private void printMessage(String message){
+        if (friendlyInterface) {
+            System.out.println(message);
+        }
+    }
 }
