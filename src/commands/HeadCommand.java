3379c5858086509fc4fede5f8a755ae36536a253
apchhiv <ira.karaseva.andre@gmail.com>
2022-03-17 21:17:51 +0300

    команда head

---
+++ src/commands/HeadCommand.java
@@ -0,0 +1,28 @@
+package commands;
+
+import handlers.InputHandler;
+import source.HumanBeing;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Класс команды head
+ * Выводит первый элемент коллекции
+ */
+public class HeadCommand implements Command{
+    private Deque<HumanBeing> humanCollection = new ArrayDeque<>();
+
+    HeadCommand(Deque<HumanBeing> humanCollection){
+        this.humanCollection = humanCollection;
+    }
+
+    @Override
+    public void execute(InputHandler reader) {
+        if (!(humanCollection.isEmpty())){
+            System.out.print("head: Первый элемент коллекции - " + humanCollection.getFirst());
+        } else {
+            System.err.print("head: Sorry, коллекция пуста.\n");
+        }
+    }
+}
