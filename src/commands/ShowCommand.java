71b73e1ad7589ecc15f7ee82284d8723fe9b528e
apchhiv <ira.karaseva.andre@gmail.com>
2022-03-17 21:18:02 +0300

    команда show

---
+++ src/commands/ShowCommand.java
@@ -0,0 +1,32 @@
+package commands;
+
+import dao.DAO;
+import handlers.InputHandler;
+import source.HumanBeing;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Класс команды show
+ * Выводит в стандартный поток вывода все элементы коллекции в строковом представлении
+ */
+public class ShowCommand implements Command{
+    private Deque<HumanBeing> humanCollection = new ArrayDeque<>();
+    private final DAO arrayDequeDAO;
+
+    ShowCommand(Deque<HumanBeing> humanCollection, DAO arrayDequeDAO){
+        this.humanCollection = humanCollection;
+        this.arrayDequeDAO = arrayDequeDAO;
+    }
+
+    @Override
+    public void execute(InputHandler reader) {
+        int id = humanCollection.getLast().getId();
+        for (int i=0; i<id; i++){
+            if (arrayDequeDAO.get(i) != null){
+                System.out.print(arrayDequeDAO.get(i));
+            }
+        }
+    }
+}
