2ad766806cf3c6a75b00ecafd1ad5ff4a90da1a9
apchhiv <ira.karaseva.andre@gmail.com>
2022-03-17 21:35:34 +0300

    команда remove_head

---
+++ src/commands/RemoveHeadCommand.java
@@ -0,0 +1,28 @@
+package commands;
+
+import handlers.InputHandler;
+import source.HumanBeing;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Класс команды help
+ * Выводит первый элемент коллекции и удалить его
+ */
+public class RemoveHeadCommand implements Command{
+    private Deque<HumanBeing> humanCollection = new ArrayDeque<>();
+
+    RemoveHeadCommand(Deque<HumanBeing> humanCollection){
+        this.humanCollection = humanCollection;
+    }
+
+    @Override
+    public void execute(InputHandler reader) {
+        if (!(humanCollection.isEmpty())){
+            System.out.print("remove_head: Первый элемент коллекции - " + humanCollection.removeFirst());
+        } else {
+            System.err.print("remove_head: Sorry, коллекция пуста.\n");
+        }
+    }
+}
