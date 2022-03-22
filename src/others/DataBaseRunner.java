f0d58f2edb3b22bf94f9f0dd90b00638825db860
apchhiv <ira.karaseva.andre@gmail.com>
2022-03-22 07:59:47 +0300

    все

--- src/others/DataBaseRunner.java
+++ src/others/DataBaseRunner.java
@@ -1,23 +1,9 @@
 package others;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import service.AskInput;
 import service.CommandManager;
 import service.ReaderManager;
-import source.Car;
-import source.HumanBeing;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.OutputStream;
 
 public class DataBaseRunner {
 
@@ -26,7 +12,7 @@ public class DataBaseRunner {
      *
      * @param args
      */
-    public static void main(String[] args) throws ParserConfigurationException, TransformerException {
+    public static void main(String[] args) {
         // Запрос на включение дружественного интерфейса
         AskInput.turnOnFriendly();
         // Включение считывание с консоли
