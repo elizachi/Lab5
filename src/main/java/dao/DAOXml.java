71e453731217e29ae67bb7973dea1bdfe9596c42
apchhiv <ira.karaseva.andre@gmail.com>
2022-04-02 20:48:33 +0300

    added creation file + some gradle files

--- src/main/java/dao/DAOXml.java
+++ src/main/java/dao/DAOXml.java
@@ -4,6 +4,7 @@ import source.HumanBeing;
 
 import java.io.*;
 import java.util.Deque;
+import java.util.Map;
 
 /**
  * Класс, который реализует сериализацию и десериализацию xml
@@ -12,18 +13,35 @@ public class DAOXml {
     private final DAO arrayDequeDAO;
     private Deque<HumanBeing> humanCollection;
     XmlMapper xmlMapper = new XmlMapper();
-    private static final String directory = System.getenv().get("DAO_COLLECTION_FILEPATH");
+    private static String directory;
+
+    public void setDirectory() throws IOException{
+        directory = System.getenv().get("DAO_COLLECTION_FILEPATH");
+        if (directory == null) {
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            Map environment = processBuilder.environment();
+            String path = File.pathSeparator + "HumanCollection" + File.pathSeparator + "HumanCollection.xml";
+            File file = new File(path);
+            file.createNewFile();
+            environment.put("DAO_COLLECTION_FILEPATH", path);
+        }
+    }
 
     public DAOXml(DAO arrayDequeDAO) {
         this.arrayDequeDAO = arrayDequeDAO;
+        try {
+            setDirectory();
+        } catch (IOException e) {
+            e.getMessage();
+        }
     }
 
     public void serialize() throws IOException {
-        xmlMapper.writeValue(new File(directory + "HumanCollection.xml"), arrayDequeDAO.getAll());
+        xmlMapper.writeValue(new File(directory), arrayDequeDAO.getAll());
     }
 
     public Deque<HumanBeing> deserialize() throws IOException {
-        File file = new File(directory + "HumanCollection.xml");
+        File file = new File(directory);
         String xml = bufferedInputToString(new FileInputStream(file));
         Deque value = xmlMapper.readValue(xml, humanCollection.getClass());
         return value;
